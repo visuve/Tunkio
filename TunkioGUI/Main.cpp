75a8e38ef352e8bc6f459538c0aed18f2f55b405
visuve <veli-matti.visuri@iki.fi>
2020-05-21 23:56:03 +0300

    Change GUI to FLTK

--- TunkioGUI/Main.cpp
+++ TunkioGUI/Main.cpp
@@ -1,17 +1,69 @@
 #include "PCH.hpp"
-#include "TunkioProgressDialog.hpp"
 
-int APIENTRY wWinMain(
-    _In_ HINSTANCE instance,
-    _In_opt_ HINSTANCE previousInstance,
-    _In_ LPWSTR commandLine,
-    _In_ int showCommand)
+namespace
 {
-    UNREFERENCED_PARAMETER(previousInstance);
-    UNREFERENCED_PARAMETER(showCommand);
+    constexpr int WindowWidth = 500;
+    constexpr int WindowHeight = 255;
 
-    Tunkio::ProgressDialog dialog(instance, commandLine);
-    dialog.Show();
+    constexpr int LabelMargin = 10;
+    constexpr int LabelSpacing = 5;
+    constexpr int LabelWidth = 480;
+    constexpr int LabelHeight = 30;
 
-    return 0;
+    constexpr int ButtonWidth = 100;
+
+    void OnRunButtonClicked(Fl_Widget*, void*)
+    {
+        std::cout << "RUN!" << std::endl;
+    }
+
+    void OnAbortButtonClicked(Fl_Widget*, void*)
+    {
+        std::cout << "ABORT!" << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) 
+{
+    auto window = new Fl_Window(WindowWidth, WindowHeight, "Tunkio");
+
+    {
+        auto cmdParams = new Fl_Box(FL_EMBOSSED_FRAME, LabelMargin, LabelSpacing, LabelWidth, LabelHeight, "CMD PARAMS:");
+        cmdParams->align(Fl_Align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE));
+
+        auto dataWrittenLabel = new Fl_Box(FL_EMBOSSED_FRAME, LabelMargin, LabelHeight + LabelSpacing * 2, LabelWidth, LabelHeight, "WRITTEN:");
+        dataWrittenLabel->align(Fl_Align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE));
+
+        auto speedLabel = new Fl_Box(FL_EMBOSSED_FRAME, LabelMargin, LabelHeight * 2 + LabelSpacing * 3, LabelWidth, LabelHeight, "SPEED:");
+        speedLabel->align(Fl_Align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE));
+
+        auto avgSpeedLabel = new Fl_Box(FL_EMBOSSED_FRAME, LabelMargin, LabelHeight * 3 + LabelSpacing * 4, LabelWidth, LabelHeight, "AVG. SPEED:");
+        avgSpeedLabel->align(Fl_Align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE));
+
+        auto timeLeftLabel = new Fl_Box(FL_EMBOSSED_FRAME, LabelMargin, LabelHeight * 4 + LabelSpacing * 5, LabelWidth, LabelHeight, "TIME LEFT:");
+        timeLeftLabel->align(Fl_Align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE));
+
+        auto progress = new Fl_Progress(LabelMargin, LabelHeight * 5 + LabelSpacing * 6, LabelWidth, LabelHeight);
+        progress->minimum(0);
+        progress->maximum(100);
+        progress->value(50.0f);
+        progress->label("50%");
+        progress->color(FL_DARK3);
+        progress->selection_color(FL_DARK_BLUE);
+        progress->labelcolor(FL_BLACK);
+
+
+        auto runButton = new Fl_Button(WindowWidth - ButtonWidth - LabelMargin, LabelHeight * 6 + LabelSpacing * 7, ButtonWidth, LabelHeight, "Run");
+        runButton->callback(OnRunButtonClicked);
+
+        auto abortButton = new Fl_Button(WindowWidth - ButtonWidth - LabelMargin - LabelSpacing - ButtonWidth, LabelHeight * 6 + LabelSpacing * 7, 100, LabelHeight, "Abort");
+        abortButton->callback(OnAbortButtonClicked);
+
+
+    }
+
+    window->end();
+    window->show(argc, argv);
+
+    return Fl::run();
 }
--- TunkioGUI/PCH.hpp
+++ TunkioGUI/PCH.hpp
@@ -1,25 +1,15 @@
 #pragma once
 
-#include <SDKDDKVer.h>
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#pragma warning(push)
+#pragma warning(disable: 4244)
+#pragma warning(disable: 4458)
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.h>
+#include <FL/Fl_Button.h>
+#include <FL/Fl_Progress.h>
+#pragma warning(pop)
 
-// Windows Header Files
-#include <Windows.h>
-#include <CommCtrl.h>
-
-// STL
 #include <iostream>
-#include <string>
-#include <algorithm>
-#include <filesystem>
-#include <memory>
-#include <cassert>
-#include <sstream>
-#include <atomic>
-#include <csignal>
-#include <numeric>
 
-// Project
 #include "TunkioAPI.h"
-
-
